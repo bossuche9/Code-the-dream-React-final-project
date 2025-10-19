f7da0f34130f4de95fe8bef4f48830221f3459d1
bossuche9 <uchennaobi102@gmail.com>
2025-10-19 16:04:38 -0400

    feat: added .prettierrc file

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import { version } from 'jq';
 
 export default defineConfig([
   globalIgnores(['dist']),
